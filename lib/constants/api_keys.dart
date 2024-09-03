a5fe007955aaf75b8af1b8d27795094a8d375880
Mahdijamebozorg <Mahdijamebozorg@email.com>
2024-09-04 00:53:14 +0330

    including api keys

---
+++ lib/constants/api_keys.dart
@@ -0,0 +1,8 @@
+class ApiKeys {
+  ApiKeys._();
+
+  static const parseServerUrl = 'https://parseapi.back4app.com';
+  static const applicationId = '9PHviYDl0F3TAx2mEx7OljgExdB9KnbIOHf0Gjni';
+  static const clientKey = 'YbBapUIJ3N7mviiCM6Hg2eBdIFZKym8Yq2G3xKSs';
+  static const user = 'user';
+}
